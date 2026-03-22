aeb123f77d5b3a83e004062e2168dc5cd9c436b4
Surendharangithub <surenrengarajan@gmail.com>
2026-03-22 14:28:22 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { Resend } = require('resend');
+const { welcomeEmail } = require('./emails/templates/Welcome')
+
 dotenv.config()
 const app = express();
+const PORT = process.env.NODE_APP_PORT;
 
-const PORT = process.env.NODE_APP_PORT
+
+
+const resend = new Resend(process.env.NODE_APP_RESEND_API);
+
+app.post('/api/send-email', async (_, res) => {
+
+    const { data, error } = await resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: ['surendharanr.19eee@kongu.edu'],
+        subject: "With Image We're Sending in this mail!",
+        html: welcomeEmail({ name: 'Surendharan ', email: 'surendharanr.19eee@kongu.edu' }),
+    })
+
+    if (error) {
+        console.error('Error:', error);
+    } else {
+        if (data.id) {
+            res.json({
+                message: 'Email Successfully Send!'
+            })
+        }
+    }
+})
+
 
 
 app.listen(PORT, () => {
